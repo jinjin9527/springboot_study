d1bd603a915889bb5c29fae3648f1809c84b2914
sylinx_zj <sylinx_zj@163.com>
2022-07-04 23:24:02 +0900

    1

--- src/main/java/com/sylinx/springbootstudy/restcontroller/HelloWorldController.java
+++ src/main/java/com/sylinx/springbootstudy/restcontroller/HelloWorldController.java
@@ -1,12 +1,25 @@
 package com.sylinx.springbootstudy.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private ApplicationContext context;
+
     @GetMapping("/helloworld")
     public String helloWorld(){
         return "Hello World!123456";
+    }
+
+    @GetMapping("/shutdown")
+    public void shutdownContext() {
+        ((ConfigurableApplicationContext) context).close();
     }
 }
